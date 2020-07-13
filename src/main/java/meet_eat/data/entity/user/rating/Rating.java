5d5d698b785da7b02642a778a9289681927deba2
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-14 01:44:34 +0200

    Add Rating class

--- src/main/java/meet_eat/data/entity/user/rating/Rating.java
+++ src/main/java/meet_eat/data/entity/user/rating/Rating.java
@@ -1,6 +1,41 @@
 package meet_eat.data.entity.user.rating;
 
+import java.util.Objects;
+
 public class Rating {
 
-    
+    private static final String ERROR_MESSAGE_TEMPLATE = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_BASIS = String.format(ERROR_MESSAGE_TEMPLATE, "basis");
+    private static final String ERROR_MESSAGE_NULL_VALUE = String.format(ERROR_MESSAGE_TEMPLATE, "value");
+    private static final String ERROR_MESSAGE_NULL_REVIEWER = String.format(ERROR_MESSAGE_TEMPLATE, "reviewer");
+
+    private final RatingBasis basis;
+    private final RatingValue value;
+    private final User reviewer;
+
+    public Rating(RatingBasis basis, RatingValue value, User reviewer) {
+
+        Objects.requireNonNull(basis, ERROR_MESSAGE_NULL_BASIS);
+        Objects.requireNonNull(value, ERROR_MESSAGE_NULL_VALUE);
+        Objects.requireNonNull(reviewer, ERROR_MESSAGE_NULL_REVIEWER);
+
+        this.basis = basis;
+        this.value = value;
+        this.reviewer = reviewer;
+    }
+
+    public RatingBasis getBasis() {
+
+        return basis;
+    }
+
+    public RatingValue getValue() {
+
+        return value;
+    }
+
+    public User getReviewer() {
+
+        return reviewer;
+    }
 }
