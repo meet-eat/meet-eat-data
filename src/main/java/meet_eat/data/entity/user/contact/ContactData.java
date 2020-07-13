32fed3e418f840ecada7ad4fdc6011af38a105ef
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-14 01:41:52 +0200

    Add requireNonNull statement

--- src/main/java/meet_eat/data/entity/user/contact/ContactData.java
+++ src/main/java/meet_eat/data/entity/user/contact/ContactData.java
@@ -3,13 +3,19 @@ package meet_eat.data.entity.user.contact;
 import java.util.Collections;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class ContactData {
     
+    private static final String ERROR_MESSAGE_TEMPLATE = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_REQUEST = String.format(ERROR_MESSAGE_TEMPLATE, "request");
+
     private final Map<ContactType, String> contacts;
     private final ContactRequest request;
 
     public ContactData(ContactRequest request) {
+
+        Objects.requireNonNull(request, ERROR_MESSAGE_NULL_REQUEST);
 
         this.contacts = new EnumMap<>(ContactType.class);
         this.request = request;
--- src/main/java/meet_eat/data/entity/user/contact/ContactRequest.java
+++ src/main/java/meet_eat/data/entity/user/contact/ContactRequest.java
@@ -1,11 +1,20 @@
 package meet_eat.data.entity.user.contact;
 
+import java.util.Objects;
+
 public class ContactRequest {
     
+    private static final String ERROR_MESSAGE_TEMPLATE = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_REQUESTER = String.format(ERROR_MESSAGE_TEMPLATE, "requester");
+    private static final String ERROR_MESSAGE_NULL_REQUESTED_USER = String.format(ERROR_MESSAGE_TEMPLATE, "requestedUser");
+
     private final User requester;
     private final User requestedUser;
 
     public ContactRequest(User requester, User requestedUser) {
+
+        Objects.requireNonNull(requester, ERROR_MESSAGE_NULL_REQUESTER);
+        Objects.requireNonNull(requestedUser, ERROR_MESSAGE_NULL_REQUESTED_USER);
 
         this.requester = requester;
         this.requestedUser = requestedUser;
