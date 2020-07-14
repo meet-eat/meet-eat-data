892ee29514428ac402b8742dd2d56b7cb08c8d1d
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-14 20:30:20 +0200

    Add integer values to RatingValue
    - Including attribute, constructor and getter

--- src/main/java/meet_eat/data/entity/user/rating/RatingValue.java
+++ src/main/java/meet_eat/data/entity/user/rating/RatingValue.java
@@ -2,13 +2,23 @@ package meet_eat.data.entity.user.rating;
 
 public enum RatingValue {
 
-    POINTS_1,
+    POINTS_1(1),
 
-    POINTS_2,
+    POINTS_2(2),
 
-    POINTS_3,
+    POINTS_3(3),
 
-    POINTS_4,
+    POINTS_4(4),
 
-    POINTS_5;
+    POINTS_5(5);
+
+    private int integerValue;
+
+    private RatingValue(int integerValue) {
+        this.integerValue = integerValue;
+    }
+
+    public int getIntegerValue() {
+        return integerValue;
+    }
 }
