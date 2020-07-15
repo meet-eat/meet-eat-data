d644feeb20c2f57246967cc37053ccebd364a0a4
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-15 16:04:05 +0200

    Add CoordinateLocation class

--- src/main/java/meet_eat/data/location/CoordinateLocation.java
+++ src/main/java/meet_eat/data/location/CoordinateLocation.java
@@ -1,5 +1,22 @@
 package meet_eat.data.location;
 
-public class CoordinateLocation {
+public class CoordinateLocation implements Localizable {
     
+    private static final String ERROR_MESSAGE_NOT_IMPLEMENTED = "This function is not implemented yet.";
+
+    private final Coordinate coordinate;
+
+    public CoordinateLocation(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    @Override
+    public double getDistance(Localizable location) {
+        // TODO Implement
+        throw new UnsupportedOperationException(ERROR_MESSAGE_NOT_IMPLEMENTED);
+    }
 }
