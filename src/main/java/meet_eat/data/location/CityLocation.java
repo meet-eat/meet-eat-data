39b7a9e457567dab0cfae0f41489cf17ba43a5d7
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-15 16:03:41 +0200

    Add CityLocation class

--- src/main/java/meet_eat/data/location/CityLocation.java
+++ src/main/java/meet_eat/data/location/CityLocation.java
@@ -1,5 +1,28 @@
 package meet_eat.data.location;
 
-public class CityLocation {
+public class CityLocation implements Localizable {
     
+    private static final String ERROR_MESSAGE_NOT_IMPLEMENTED = "This function is not implemented yet.";
+
+    private final String city;
+
+    public CityLocation(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public double getDistance(Localizable location) {
+        // TODO Implement
+        throw new UnsupportedOperationException(ERROR_MESSAGE_NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public Coordinate getCoordinate() {
+        // TODO Implement
+        throw new UnsupportedOperationException(ERROR_MESSAGE_NOT_IMPLEMENTED);
+    }
 }
