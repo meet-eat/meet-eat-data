be4f1fd7e5b964c536b6b2b666776898816b8899
Moritz Gstuer <moritz@gstuer.com>
2020-07-15 19:19:25 +0200

    Fix imports of user class

--- src/main/java/meet_eat/data/entity/user/User.java
+++ src/main/java/meet_eat/data/entity/user/User.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -14,6 +13,7 @@ import java.util.stream.Collectors;
 import meet_eat.data.entity.Offer;
 import meet_eat.data.entity.user.rating.Rating;
 import meet_eat.data.entity.user.rating.RatingBasis;
+import meet_eat.data.entity.user.setting.Setting;
 
 public class User {
     
