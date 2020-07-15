bc5ace3224d0a0dd59dd8f563a4503a14d1f9647
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-15 16:03:54 +0200

    Add Coordinate class

--- src/main/java/meet_eat/data/location/Coordinate.java
+++ src/main/java/meet_eat/data/location/Coordinate.java
@@ -1,5 +1,20 @@
 package meet_eat.data.location;
 
 public class Coordinate {
-    
+
+    private double longitude;
+    private double latitude;
+
+    public Coordinate(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
 }
