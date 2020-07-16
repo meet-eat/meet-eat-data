b29620b4dcea8de73c8e0476851bb1b28c22e0a0
Moritz Gstuer <moritz@gstuer.com>
2020-07-16 22:14:59 +0200

    Add test class for abstract class entity

---
+++ src/test/java/meet_eat/data/entity/EntityCommonTest.java
@@ -0,0 +1,41 @@
+package meet_eat.data.entity;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class EntityCommonTest {
+
+    private class ConcreteEntity extends Entity {
+
+        public ConcreteEntity() {
+            super();
+        }
+
+        public ConcreteEntity(String identifier) {
+            super(identifier);
+        }
+    }
+
+    @Test
+    public void testConstructorEmpty() {   
+        // Execution
+        ConcreteEntity entity = new ConcreteEntity();
+
+        // Assertions
+        assertNotNull(entity);
+    }
+
+    @Test
+    public void testConstructorIdentifier() {   
+        // Test data
+        String identifier = "IdentifierTest1234!";
+
+        // Execution
+        ConcreteEntity entity = new ConcreteEntity(identifier);
+
+        // Assertions
+        assertEquals(identifier, entity.getIdentifier());
+    }
+}
