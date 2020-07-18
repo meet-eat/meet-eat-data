0b16ef0a206ce13bb8b5545d54ec13d11647d311
Moritz Gstuer <moritz@gstuer.com>
2020-07-18 16:30:15 +0200

    Implement Localizable interface behavior

--- src/main/java/meet_eat/data/location/Localizable.java
+++ src/main/java/meet_eat/data/location/Localizable.java
@@ -1,8 +1,12 @@
 package meet_eat.data.location;
 
+import meet_eat.data.location.geometry.Haversine;
+
 public interface Localizable {
-    
-    public double getDistance(Localizable location);
-    
-    public Coordinate getCoordinate();
+
+    public Coordinate getCoordinate() throws UnlocalizableException;
+
+    public default double getDistance(Localizable localizable) throws UnlocalizableException {
+        return Haversine.applyHaversineFormula(this.getCoordinate(), localizable.getCoordinate());
+    }
 }
---
+++ src/main/java/meet_eat/data/location/UnlocalizableException.java
@@ -0,0 +1,14 @@
+package meet_eat.data.location;
+
+public class UnlocalizableException extends Exception {
+
+    private static final long serialVersionUID = -8479470469258687628L;
+
+    public UnlocalizableException() {
+        super();
+    }
+    
+    public UnlocalizableException(String message) {
+        super(message);
+    }
+}
