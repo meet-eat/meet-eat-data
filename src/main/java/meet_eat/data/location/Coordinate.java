9a23f09e7daadcdb622a43a5e30fbfd691885470
Moritz Gstuer <moritz@gstuer.com>
2020-07-18 14:59:47 +0200

    Add functionality to coordinate class
    Additionally, add jackson annotations to the class.

--- src/main/java/meet_eat/data/location/Coordinate.java
+++ src/main/java/meet_eat/data/location/Coordinate.java
@@ -1,20 +1,44 @@
 package meet_eat.data.location;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Coordinate {
 
-    private double longitude;
     private double latitude;
+    private double longitude;
 
-    public Coordinate(double longitude, double latitude) {
-        this.longitude = longitude;
+    @JsonCreator
+    public Coordinate(
+            @JsonProperty("lat") double latitude,
+            @JsonProperty("lon") double longitude) {
         this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLatitudeAsRadians() {
+        return Math.toRadians(latitude);
     }
 
     public double getLongitude() {
         return longitude;
     }
 
-    public double getLatitude() {
-        return latitude;
+    public double getLongitudeAsRadians() {
+        return Math.toRadians(latitude);
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 }
