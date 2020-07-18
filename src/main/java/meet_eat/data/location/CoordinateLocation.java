546071cd473afe3b1a415ee373eba3fe9c5e6907
Moritz Gstuer <moritz@gstuer.com>
2020-07-18 16:33:26 +0200

    Add functionality to CoordinateLocation

--- src/main/java/meet_eat/data/location/CoordinateLocation.java
+++ src/main/java/meet_eat/data/location/CoordinateLocation.java
@@ -1,22 +1,19 @@
 package meet_eat.data.location;
 
 public class CoordinateLocation implements Localizable {
-    
-    private static final String ERROR_MESSAGE_NOT_IMPLEMENTED = "This function is not implemented yet.";
 
-    private final Coordinate coordinate;
+    private Coordinate coordinate;
 
     public CoordinateLocation(Coordinate coordinate) {
         this.coordinate = coordinate;
     }
 
+    @Override
     public Coordinate getCoordinate() {
         return coordinate;
     }
 
-    @Override
-    public double getDistance(Localizable location) {
-        // TODO Implement
-        throw new UnsupportedOperationException(ERROR_MESSAGE_NOT_IMPLEMENTED);
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
     }
 }
