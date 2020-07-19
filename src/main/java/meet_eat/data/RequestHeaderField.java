567cbe955a522a538ea726bfa7f21d820882b1e6
Moritz Gstuer <moritz@gstuer.com>
2020-07-19 23:44:11 +0200

    Add enum for request header fields

---
+++ src/main/java/meet_eat/data/RequestHeaderField.java
@@ -0,0 +1,23 @@
+package meet_eat.data;
+
+public enum RequestHeaderField {
+
+    TOKEN,
+    PAGE,
+    PREDICATES,
+    COMPARATORS;
+
+    private final String identifier;
+
+    private RequestHeaderField() {
+        this.identifier = this.name().toLowerCase();
+    }
+
+    private RequestHeaderField(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getIdentifier() {
+        return this.name().toLowerCase();
+    }
+}
