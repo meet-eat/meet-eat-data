26ebb691c81047fff3af7490e94e03bc39f2e0cc
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-21 17:34:37 +0200

    Add non-null check for reports

--- src/main/java/meet_eat/data/entity/ReportableEntity.java
+++ src/main/java/meet_eat/data/entity/ReportableEntity.java
@@ -3,11 +3,15 @@ package meet_eat.data.entity;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Objects;
 
 import meet_eat.data.Report;
 
 public abstract class ReportableEntity extends Entity {
-    
+
+    private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_REPORTS = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "reports");
+
     private final Collection<Report> reports;
 
     protected ReportableEntity() {
@@ -17,6 +21,7 @@ public abstract class ReportableEntity extends Entity {
 
     protected ReportableEntity(String identifier, Collection<Report> reports) {
         super(identifier);
+        Objects.requireNonNull(reports, ERROR_MESSAGE_NULL_REPORTS);
         this.reports = reports;
     }
 
