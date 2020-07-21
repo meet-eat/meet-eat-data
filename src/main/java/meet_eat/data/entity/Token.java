7bbaf97171dc4046e8bb4ecc6838b0b99925ddc3
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-21 22:42:40 +0200

    Add non-null check for user and value

--- src/main/java/meet_eat/data/entity/Token.java
+++ src/main/java/meet_eat/data/entity/Token.java
@@ -2,18 +2,31 @@ package meet_eat.data.entity;
 
 import meet_eat.data.entity.user.User;
 
+import java.util.Objects;
+
 public class Token extends Entity {
-    
+
+    private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_USER = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "user");
+    private static final String ERROR_MESSAGE_NULL_VALUE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "value");
+
     private final User user;
     private final String value;
 
     public Token(User user, String value) {
+        Objects.requireNonNull(user, ERROR_MESSAGE_NULL_USER);
+        Objects.requireNonNull(value, ERROR_MESSAGE_NULL_VALUE);
+
         this.user = user;
         this.value = value;
     }
 
     public Token(String identifier, User user, String value) {
         super(identifier);
+
+        Objects.requireNonNull(user, ERROR_MESSAGE_NULL_USER);
+        Objects.requireNonNull(value, ERROR_MESSAGE_NULL_VALUE);
+
         this.user = user;
         this.value = value;
     }
