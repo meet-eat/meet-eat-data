5c76538f4b243192aba114b25960e0081c6b5413
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-21 22:50:09 +0200

    Add non-null check for spherical position

--- src/main/java/meet_eat/data/location/SphericalLocation.java
+++ src/main/java/meet_eat/data/location/SphericalLocation.java
@@ -1,10 +1,16 @@
 package meet_eat.data.location;
 
+import java.util.Objects;
+
 public class SphericalLocation implements Localizable {
+
+    private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_SPHERICAL_POSITION = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "sphericalPosition");
 
     private SphericalPosition sphericalPosition;
 
     public SphericalLocation(SphericalPosition sphericalPosition) {
+        Objects.requireNonNull(sphericalPosition, ERROR_MESSAGE_NULL_SPHERICAL_POSITION);
         this.sphericalPosition = sphericalPosition;
     }
 
@@ -14,6 +20,7 @@ public class SphericalLocation implements Localizable {
     }
 
     public void setSphericalPosition(SphericalPosition sphericalPosition) {
+        Objects.requireNonNull(sphericalPosition, ERROR_MESSAGE_NULL_SPHERICAL_POSITION);
         this.sphericalPosition = sphericalPosition;
     }
 }
