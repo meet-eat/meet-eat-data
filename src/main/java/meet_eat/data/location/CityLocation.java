6f50bb5dae25ceaa4c1797c6c6e50378c8677b0f
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-21 22:52:04 +0200

    Add non-null check for city name

--- src/main/java/meet_eat/data/location/CityLocation.java
+++ src/main/java/meet_eat/data/location/CityLocation.java
@@ -6,11 +6,14 @@ import java.util.Objects;
 
 public class CityLocation implements Localizable {
 
+    private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_CITY_NAME = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "cityName");
     private static final String UNLOCALIZABLE_CITY = "The given city is not localizable.";
 
     private String cityName;
 
     public CityLocation(String cityName) {
+        Objects.requireNonNull(cityName, ERROR_MESSAGE_NULL_CITY_NAME);
         this.cityName = cityName;
     }
 
@@ -19,6 +22,7 @@ public class CityLocation implements Localizable {
     }
 
     public void setCityName(String cityName) {
+        Objects.requireNonNull(cityName, ERROR_MESSAGE_NULL_CITY_NAME);
         this.cityName = cityName;
     }
 
