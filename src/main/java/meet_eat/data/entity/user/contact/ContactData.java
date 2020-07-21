51e7321784abee4303b1316d554327d4214d6af1
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-21 22:43:39 +0200

    Add non-null check for contact type and value

--- src/main/java/meet_eat/data/entity/user/contact/ContactData.java
+++ src/main/java/meet_eat/data/entity/user/contact/ContactData.java
@@ -9,13 +9,14 @@ public class ContactData {
     
     private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
     private static final String ERROR_MESSAGE_NULL_REQUEST = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "request");
+    private static final String ERROR_MESSAGE_NULL_CONTACT_TYPE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "contactType");
+    private static final String ERROR_MESSAGE_NULL_VALUE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "value");
 
     private final Map<ContactType, String> contacts;
     private final ContactRequest request;
 
     public ContactData(ContactRequest request) {
         Objects.requireNonNull(request, ERROR_MESSAGE_NULL_REQUEST);
-        
         contacts = new EnumMap<>(ContactType.class);
         this.request = request;
     }
@@ -33,6 +34,8 @@ public class ContactData {
     }
 
     public void putContact(ContactType type, String value) {
+        Objects.requireNonNull(type, ERROR_MESSAGE_NULL_CONTACT_TYPE);
+        Objects.requireNonNull(value, ERROR_MESSAGE_NULL_VALUE);
         contacts.put(type, value);
     }
 }
