6b1d445fd8d7db4ee2041128b875511d3876a47f
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-21 18:53:18 +0200

    Add non-null check for name

--- src/main/java/meet_eat/data/entity/Tag.java
+++ src/main/java/meet_eat/data/entity/Tag.java
@@ -1,15 +1,22 @@
 package meet_eat.data.entity;
 
+import java.util.Objects;
+
 public class Tag extends Entity {
-    
+
+    private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_NAME = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "name");
+
     private String name;
 
     public Tag(String name) {
+        Objects.requireNonNull(name, ERROR_MESSAGE_NULL_NAME);
         this.name = name;
     }
 
     public Tag(String identifier, String name) {
         super(identifier);
+        Objects.requireNonNull(name, ERROR_MESSAGE_NULL_NAME);
         this.name = name;
     }
 
@@ -18,6 +25,7 @@ public class Tag extends Entity {
     }
 
     public void setName(String name) {
+        Objects.requireNonNull(name, ERROR_MESSAGE_NULL_NAME);
         this.name = name;
     }
 }
