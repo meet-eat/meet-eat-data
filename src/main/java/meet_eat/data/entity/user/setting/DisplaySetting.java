b7fe8ed71605db8631781eaab4f780d22998b401
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-21 22:47:21 +0200

    Add non-null check for color mode

--- src/main/java/meet_eat/data/entity/user/setting/DisplaySetting.java
+++ src/main/java/meet_eat/data/entity/user/setting/DisplaySetting.java
@@ -1,8 +1,12 @@
 package meet_eat.data.entity.user.setting;
 
+import java.util.Objects;
+
 public class DisplaySetting implements Setting {
 
     private static final String ERROR_MESSAGE_NOT_IMPLEMENTED = "This function is not implemented yet.";
+    private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
+    private static final String ERROR_MESSAGE_NULL_COLOR_MODE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "colorMode");
     private static final ColorMode COLOR_MODE_DEFAULT = ColorMode.DARK;
 
     private ColorMode colorMode;
@@ -12,6 +16,7 @@ public class DisplaySetting implements Setting {
     }
 
     public DisplaySetting(ColorMode colorMode) {
+        Objects.requireNonNull(colorMode, ERROR_MESSAGE_NULL_COLOR_MODE);
         this.colorMode = colorMode;
     }
 
@@ -20,6 +25,7 @@ public class DisplaySetting implements Setting {
     }
 
     public void setColorMode(ColorMode colorMode) {
+        Objects.requireNonNull(colorMode, ERROR_MESSAGE_NULL_COLOR_MODE);
         this.colorMode = colorMode;
     }
 
