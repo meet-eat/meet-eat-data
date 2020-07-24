ee199f50f93df816aac9ff057ee927f2d96b239b
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-24 20:21:58 +0200

    Add test for LoginCredential
    Note: Apply method for password class not implemented yet

---
+++ src/test/java/meet_eat/data/LoginCredentialCommonTest.java
@@ -0,0 +1,46 @@
+package meet_eat.data;
+
+import meet_eat.data.entity.user.Email;
+import meet_eat.data.entity.user.Password;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginCredentialCommonTest {
+
+    @Test
+    public void testConstructor() {
+        // Test data
+        String testEmail = "test@meet-eat.com";
+        Email email = new Email(testEmail);
+        String testPassword = "MySpecial*Password1";
+        Password password = new Password(testPassword);
+
+        // Execution
+        LoginCredential loginCredential = new LoginCredential(email, password);
+
+        // Assertions
+        assertEquals(email, loginCredential.getEmail());
+        assertEquals(password, loginCredential.getPassword());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorNullEmail() {
+        // Test data
+        String testPassword = "MySpecial*Password1";
+        Password password = new Password(testPassword);
+
+        // Execution
+        LoginCredential loginCredential = new LoginCredential(null, password);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorNullPassword() {
+        // Test data
+        String testEmail = "test@meet.eat.com";
+        Email email = new Email(testEmail);
+
+        // Execution
+        LoginCredential loginCredential = new LoginCredential(email, null);
+    }
+}
