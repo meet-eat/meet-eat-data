f9701a0c15d56bd50b74adefc261b2803d449793
Moritz Gstuer <moritz@gstuer.com>
2020-07-27 21:11:34 +0200

    Add annotations and equals to report class

--- src/main/java/meet_eat/data/Report.java
+++ src/main/java/meet_eat/data/Report.java
@@ -2,6 +2,10 @@ package meet_eat.data;
 
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import meet_eat.data.entity.user.User;
 
 public class Report {
@@ -10,28 +14,51 @@ public class Report {
     private static final String ERROR_MESSAGE_NULL_REPORTER = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "reporter");
     private static final String ERROR_MESSAGE_NULL_MESSAGE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "message");
 
+    @JsonProperty
     private final User reporter;
+    @JsonProperty
     private final String message;
+    @JsonProperty
     private boolean processed;
 
-    public Report(User reporter, String message) {
+    @JsonCreator
+    public Report(@JsonProperty("reporter") User reporter, @JsonProperty("message") String message) {
         this.reporter = Objects.requireNonNull(reporter, ERROR_MESSAGE_NULL_REPORTER);
         this.message = Objects.requireNonNull(message, ERROR_MESSAGE_NULL_MESSAGE);
     }
 
+    @JsonGetter
     public User getReporter() {
         return reporter;
     }
-    
+
+    @JsonGetter
     public String getMessage() {
         return message;
     }
 
+    @JsonGetter
     public boolean isProcessed() {
         return processed;
     }
 
-    public void setProcessed(boolean processed) {
+    @JsonSetter("processed")
+    public void setProcessed(@JsonProperty("processed") boolean processed) {
         this.processed = processed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return processed == report.processed &&
+                Objects.equals(reporter, report.reporter) &&
+                Objects.equals(message, report.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reporter, message, processed);
     }
 }
