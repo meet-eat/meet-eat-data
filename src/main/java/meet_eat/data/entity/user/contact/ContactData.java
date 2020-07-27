1df102bd5c308771f31b17e8b4c7d4bfeeabffd4
Moritz Gstuer <moritz@gstuer.com>
2020-07-27 19:34:54 +0200

    Add annotations and equals to ContactData class

--- src/main/java/meet_eat/data/entity/user/contact/ContactData.java
+++ src/main/java/meet_eat/data/entity/user/contact/ContactData.java
@@ -1,5 +1,10 @@
 package meet_eat.data.entity.user.contact;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Collections;
 import java.util.EnumMap;
 import java.util.Map;
@@ -12,22 +17,32 @@ public class ContactData {
     private static final String ERROR_MESSAGE_NULL_CONTACT_TYPE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "contactType");
     private static final String ERROR_MESSAGE_NULL_VALUE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "value");
 
+    @JsonProperty
     private final Map<ContactType, String> contacts;
+    @JsonProperty
     private final ContactRequest request;
 
     public ContactData(ContactRequest request) {
         contacts = new EnumMap<>(ContactType.class);
-        this.request = Objects.requireNonNull(request, ERROR_MESSAGE_NULL_REQUEST);;
+        this.request = Objects.requireNonNull(request, ERROR_MESSAGE_NULL_REQUEST);
+    }
+
+    @JsonCreator
+    public ContactData(ContactRequest request, Map<ContactType, String> contacts) {
+        this.contacts = Objects.requireNonNull(contacts);
+        this.request = Objects.requireNonNull(request, ERROR_MESSAGE_NULL_REQUEST);
     }
 
     public Map<ContactType, String> getContacts() {
         return Collections.unmodifiableMap(contacts);
     }
 
+    @JsonIgnore
     public String getContactByType(ContactType type){
         return contacts.get(type);
     }
 
+    @JsonGetter
     public ContactRequest getRequest() {
         return request;
     }
@@ -36,5 +51,18 @@ public class ContactData {
         Objects.requireNonNull(type, ERROR_MESSAGE_NULL_CONTACT_TYPE);
         Objects.requireNonNull(value, ERROR_MESSAGE_NULL_VALUE);
         contacts.put(type, value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return Objects.equals(contacts, that.contacts) && Objects.equals(request, that.request);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contacts, request);
     }
 }
