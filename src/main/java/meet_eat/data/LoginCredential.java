41a8ea62935f00808a7e0efacd7f1d47340d7170
Moritz Gstuer <moritz@gstuer.com>
2020-07-27 19:50:31 +0200

    Add annotations and equals to LoginCredential

--- src/main/java/meet_eat/data/LoginCredential.java
+++ src/main/java/meet_eat/data/LoginCredential.java
@@ -1,5 +1,8 @@
 package meet_eat.data;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import meet_eat.data.entity.user.Email;
 import meet_eat.data.entity.user.Password;
 
@@ -11,19 +14,38 @@ public class LoginCredential {
     private static final String ERROR_MESSAGE_NULL_EMAIL = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "email address");
     private static final String ERROR_MESSAGE_NULL_PASSWORD = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "password");
 
+    @JsonProperty
     private final Email email;
+    @JsonProperty
     private final Password password;
 
-    public LoginCredential(Email email, Password password) {
+    @JsonCreator
+    public LoginCredential(@JsonProperty("email") Email email, @JsonProperty("password") Password password) {
         this.email = Objects.requireNonNull(email, ERROR_MESSAGE_NULL_EMAIL);
         this.password = Objects.requireNonNull(password, ERROR_MESSAGE_NULL_PASSWORD);
     }
 
+    @JsonGetter
     public Email getEmail() {
         return email;
     }
 
+    @JsonGetter
     public Password getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LoginCredential that = (LoginCredential) o;
+        return Objects.equals(email, that.email) &&
+                Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
     }
 }
