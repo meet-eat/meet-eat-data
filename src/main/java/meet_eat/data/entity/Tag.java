809ef683ce1d39147f1829759cbec06506531bd2
Moritz Gstuer <moritz@gstuer.com>
2020-07-27 20:20:13 +0200

    Add annotations and equals to tag class

--- src/main/java/meet_eat/data/entity/Tag.java
+++ src/main/java/meet_eat/data/entity/Tag.java
@@ -1,5 +1,9 @@
 package meet_eat.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Objects;
 
 public class Tag extends Entity<String> {
@@ -7,22 +11,39 @@ public class Tag extends Entity<String> {
     private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
     private static final String ERROR_MESSAGE_NULL_NAME = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "name");
 
+    @JsonProperty
     private String name;
 
     public Tag(String name) {
         this.name = Objects.requireNonNull(name, ERROR_MESSAGE_NULL_NAME);
     }
 
-    public Tag(String identifier, String name) {
+    @JsonCreator
+    public Tag(@JsonProperty("identifier") String identifier, @JsonProperty("name") String name) {
         super(identifier);
         this.name = Objects.requireNonNull(name, ERROR_MESSAGE_NULL_NAME);
     }
 
+    @JsonGetter
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = Objects.requireNonNull(name, ERROR_MESSAGE_NULL_NAME);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(name, tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name);
     }
 }
