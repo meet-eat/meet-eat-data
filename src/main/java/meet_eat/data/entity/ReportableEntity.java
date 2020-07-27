e1870386f2c2340b6e7c9c50b6482e13c28c63b6
Moritz Gstuer <moritz@gstuer.com>
2020-07-27 20:48:15 +0200

    Add annotations and equals to ReportableEntity

--- src/main/java/meet_eat/data/entity/ReportableEntity.java
+++ src/main/java/meet_eat/data/entity/ReportableEntity.java
@@ -5,14 +5,26 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import meet_eat.data.Report;
+import meet_eat.data.entity.user.User;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = User.class),
+        @JsonSubTypes.Type(value = Offer.class),
+})
 public abstract class ReportableEntity<U> extends Entity<U> {
 
     private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
     private static final String ERROR_MESSAGE_NULL_REPORTS = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "reports");
     private static final String ERROR_MESSAGE_NULL_REPORT = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "report");
 
+    @JsonProperty
     private final Collection<Report> reports;
 
     protected ReportableEntity() {
@@ -20,16 +32,32 @@ public abstract class ReportableEntity<U> extends Entity<U> {
         this.reports = new LinkedList<>();
     }
 
-    protected ReportableEntity(U identifier, Collection<Report> reports) {
+    @JsonCreator
+    protected ReportableEntity(@JsonProperty("identifier") U identifier, @JsonProperty("reports") Collection<Report> reports) {
         super(identifier);
         this.reports = Objects.requireNonNull(reports, ERROR_MESSAGE_NULL_REPORTS);
     }
 
+    @JsonGetter
     public Collection<Report> getReports() {
         return Collections.unmodifiableCollection(reports);
     }
 
     public void addReport(Report report) {
         reports.add(Objects.requireNonNull(report, ERROR_MESSAGE_NULL_REPORT));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ReportableEntity<?> that = (ReportableEntity<?>) o;
+        return Objects.equals(reports, that.reports);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), reports);
     }
 }
