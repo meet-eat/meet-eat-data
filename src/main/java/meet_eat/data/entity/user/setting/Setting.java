faadb5573c8a5eda5c1b9fcc77bb0e0cef44ae07
Moritz Gstuer <moritz@gstuer.com>
2020-07-27 21:37:20 +0200

    Add type information annotation to setting class

--- src/main/java/meet_eat/data/entity/user/setting/Setting.java
+++ src/main/java/meet_eat/data/entity/user/setting/Setting.java
@@ -1,5 +1,13 @@
 package meet_eat.data.entity.user.setting;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = DisplaySetting.class),
+        @JsonSubTypes.Type(value = NotificationSetting.class)
+})
 public interface Setting {
     
     public void apply();
