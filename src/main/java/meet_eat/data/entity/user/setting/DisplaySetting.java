6321c09d8463b318b9659d2e419477cf1012ccad
Moritz Gstuer <moritz@gstuer.com>
2020-07-27 21:40:53 +0200

    Add annotations and equals to DisplaySetting

--- src/main/java/meet_eat/data/entity/user/setting/DisplaySetting.java
+++ src/main/java/meet_eat/data/entity/user/setting/DisplaySetting.java
@@ -1,5 +1,9 @@
 package meet_eat.data.entity.user.setting;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Objects;
 
 public class DisplaySetting implements Setting {
@@ -9,16 +13,19 @@ public class DisplaySetting implements Setting {
     private static final String ERROR_MESSAGE_NULL_COLOR_MODE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "colorMode");
     private static final ColorMode COLOR_MODE_DEFAULT = ColorMode.DARK;
 
+    @JsonProperty
     private ColorMode colorMode;
 
     public DisplaySetting() {
         this.colorMode = COLOR_MODE_DEFAULT;
     }
 
-    public DisplaySetting(ColorMode colorMode) {
+    @JsonCreator
+    public DisplaySetting(@JsonProperty("colorMode") ColorMode colorMode) {
         this.colorMode = Objects.requireNonNull(colorMode, ERROR_MESSAGE_NULL_COLOR_MODE);;
     }
 
+    @JsonGetter
     public ColorMode getColorMode() {
         return colorMode;
     }
@@ -31,5 +38,18 @@ public class DisplaySetting implements Setting {
     public void apply() {
         // TODO Implement
         throw new UnsupportedOperationException(ERROR_MESSAGE_NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DisplaySetting that = (DisplaySetting) o;
+        return colorMode == that.colorMode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(colorMode);
     }
 }
