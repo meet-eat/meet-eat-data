76633f7b950fee3be0a42e4e687352121bdfa742
Moritz Gstuer <moritz@gstuer.com>
2020-07-27 19:00:34 +0200

    Add annotations and equals to email class

--- src/main/java/meet_eat/data/entity/user/Email.java
+++ src/main/java/meet_eat/data/entity/user/Email.java
@@ -1,5 +1,9 @@
 package meet_eat.data.entity.user;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Objects;
 
 public class Email {
@@ -11,9 +15,11 @@ public class Email {
             + "|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])"
             + "|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
+    @JsonProperty
     private final String address;
 
-    public Email(String address) {
+    @JsonCreator
+    public Email(@JsonProperty("address") String address) {
         if (!isLegalEmailAddress(address)) {
             throw new IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_EMAIL);
         }
@@ -28,7 +34,21 @@ public class Email {
     }
 
     @Override
+    @JsonGetter("address")
     public String toString() {
         return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email = (Email) o;
+        return Objects.equals(address, email.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address);
     }
 }
