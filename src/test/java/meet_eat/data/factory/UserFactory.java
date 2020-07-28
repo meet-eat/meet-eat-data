4eba28a0ac96b360d24b44ecef069ac2200235fd
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-28 23:10:22 +0200

    Remove SettingFactory
    Instead of using a SettingFactory, we add settings manually

--- src/test/java/meet_eat/data/factory/UserFactory.java
+++ src/test/java/meet_eat/data/factory/UserFactory.java
@@ -7,26 +7,26 @@ import meet_eat.data.entity.user.Password;
 import meet_eat.data.entity.user.Role;
 import meet_eat.data.entity.user.User;
 import meet_eat.data.entity.user.rating.Rating;
+import meet_eat.data.entity.user.setting.ColorMode;
+import meet_eat.data.entity.user.setting.DisplaySetting;
+import meet_eat.data.entity.user.setting.NotificationSetting;
 import meet_eat.data.entity.user.setting.Setting;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 
 public class UserFactory extends ObjectFactory<User> {
 
     private static final int AMOUNT_REPORTS = 2;
     private static final int AMOUNT_RATINGS = 5;
     private static final int AMOUNT_SUBSCRIPTIONS = 2;
-    private static final int AMOUNT_SETTINGS = 2;
     private static final int AMOUNT_BOOKMARKS = 2;
     private static final Role DEFAULT_ROLE = Role.USER;
+    private static final boolean DEFAULT_NOTIFICATION = false;
+    private static final boolean DEFAULT_VERIFIED = false;
 
     private ReportFactory reportFactory;
     private RatingFactory ratingFactory;
-    private SettingFactory settingFactory;
     private OfferFactory offerFactory;
     private EmailFactory emailFactory;
     private PasswordFactory passwordFactory;
@@ -35,7 +35,6 @@ public class UserFactory extends ObjectFactory<User> {
 
         reportFactory = new ReportFactory();
         ratingFactory = new RatingFactory();
-        settingFactory = new SettingFactory();
         offerFactory = new OfferFactory();
         emailFactory = new EmailFactory();
         passwordFactory = new PasswordFactory();
@@ -44,26 +43,33 @@ public class UserFactory extends ObjectFactory<User> {
     @Override
     protected User createObject() {
         String identifier = Integer.toString(objectCounter);
+
         Collection<Report> reports = new LinkedList<>();
         for (int i = 0; i < AMOUNT_REPORTS; i++) {
             reports.add(reportFactory.getValidObject());
         }
+
         Collection<Rating> ratings = new LinkedList<>();
         for (int i = 0; i < AMOUNT_RATINGS; i++) {
             ratings.add(ratingFactory.getValidObject());
         }
+
         Set<User> subscriptions = new HashSet<>();
         for (int i = 0; i < AMOUNT_SUBSCRIPTIONS; i++) {
             subscriptions.add(getValidObject());
         }
+
         Set<Setting> settings = new HashSet<>();
-        for (int i = 0; i < AMOUNT_SETTINGS; i++) {
-            settings.add(settingFactory.getValidObject());
-        }
+        Setting notificationSetting = new NotificationSetting(DEFAULT_NOTIFICATION, objectCounter);
+        Setting displaySetting = new DisplaySetting(getRandomEnumValue(ColorMode.class));
+        settings.add(notificationSetting);
+        settings.add(displaySetting);
+
         Set<Offer> bookmarks = new HashSet<>();
         for (int i = 0; i < AMOUNT_BOOKMARKS; i++) {
             bookmarks.add(offerFactory.getValidObject());
         }
+
         Role role = DEFAULT_ROLE;
         Email email = emailFactory.getValidObject();
         Password password = passwordFactory.getValidObject();
@@ -72,6 +78,12 @@ public class UserFactory extends ObjectFactory<User> {
         String phoneNumber = Integer.toString(objectCounter);
         String description = "I am " + name + " and this is my description.";
         return new User(identifier, reports, ratings, subscriptions, settings, bookmarks, role, email, password,
-                birthDay, name, phoneNumber, description, false);
+                birthDay, name, phoneNumber, description, DEFAULT_VERIFIED);
+    }
+
+    private <T extends Enum<T>> T getRandomEnumValue(Class<T> clazz) {
+        List<T> values = Arrays.asList(clazz.getEnumConstants());
+        Collections.shuffle(values);
+        return values.stream().findFirst().get();
     }
 }
