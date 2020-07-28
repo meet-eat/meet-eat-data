c165ce77c93707cbbce796b4e938abdd405b250c
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-28 23:26:27 +0200

    Add participants to factory

--- src/test/java/meet_eat/data/factory/OfferFactory.java
+++ src/test/java/meet_eat/data/factory/OfferFactory.java
@@ -12,10 +12,12 @@ import java.time.LocalTime;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 public class OfferFactory extends ObjectFactory<Offer> {
 
     private static final int AMOUNT_REPORTS = 2;
+    private static final int AMOUNT_PARTICIPANTS = 2;
     private static final int AMOUNT_TAGS = 2;
     private static final double TEST_PRICE = 5d;
     private static final int TEST_AMOUNT_PARTICIPANTS = 5;
@@ -35,22 +37,31 @@ public class OfferFactory extends ObjectFactory<Offer> {
     @Override
     protected Offer createObject() {
         String identifier = Integer.toString(objectCounter);
+
         Collection<Report> reports = new LinkedList<>();
         for (int i = 0; i < AMOUNT_REPORTS; i++) {
             reports.add(reportFactory.getValidObject());
         }
+
         User creator = userFactory.getValidObject();
-        Collection<Tag> tags = new HashSet<>();
+
+        Set<User> participants = new HashSet<>();
+        for (int i = 0; i < AMOUNT_PARTICIPANTS; i++) {
+            participants.add(userFactory.getValidObject());
+        }
+
+        Set<Tag> tags = new HashSet<>();
         for (int i = 0; i < AMOUNT_TAGS; i++) {
             tags.add(tagFactory.getValidObject());
         }
+
         String name = "TestOffer" + objectCounter;
         String description = "This is test offer number " + objectCounter;
         double price = TEST_PRICE;
         int maxParticipants = TEST_AMOUNT_PARTICIPANTS;
         LocalDateTime dateTime = LocalDateTime.of(LocalDate.EPOCH, LocalTime.NOON);
         Localizable location = locationFactory.getValidObject();
-        return new Offer(identifier, reports, creator, tags, name, description, price, maxParticipants, dateTime,
-                location);
+        return new Offer(identifier, reports, creator, participants, tags, name, description, price, maxParticipants,
+                dateTime, location);
     }
 }
