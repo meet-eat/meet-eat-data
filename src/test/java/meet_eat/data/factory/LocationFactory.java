09f2e48d3357792ac4de6af66aa385bd920ec3d7
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-28 18:57:09 +0200

    Add LocationFactory

---
+++ src/test/java/meet_eat/data/factory/LocationFactory.java
@@ -0,0 +1,33 @@
+package meet_eat.data.factory;
+
+import meet_eat.data.location.Localizable;
+import meet_eat.data.location.SphericalLocation;
+import meet_eat.data.location.SphericalPosition;
+
+import java.util.Random;
+
+public class LocationFactory extends ObjectFactory<Localizable> {
+
+    // Latitude range in degrees
+    private static final double LAT_START = -90d;
+    private static final double LAT_END = 90d;
+    // Longitude range in degrees
+    private static final double LON_START = -180d;
+    private static final double LON_END = 180d;
+
+    @Override
+    protected Localizable createObject() {
+        return new SphericalLocation(getRandomPosition());
+    }
+
+    private SphericalPosition getRandomPosition() {
+        double randomLat = new Random().nextDouble();
+        double randomLon = new Random().nextDouble();
+
+        // Create random latitude and longitude values
+        double latitude = LAT_START + (randomLat * (LAT_END - LAT_START));
+        double longitude = LON_START + (randomLon * (LON_END - LON_START));
+
+        return new SphericalPosition(latitude, longitude);
+    }
+}
