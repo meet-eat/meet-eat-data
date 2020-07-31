fc138194d92939e09b25e8ab7d6a0e8802d17c5e
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-07-31 02:23:45 +0200

    Add constructor and report add test

--- src/test/java/meet_eat/data/entity/ReportableEntityCommonTest.java
+++ src/test/java/meet_eat/data/entity/ReportableEntityCommonTest.java
@@ -1,11 +1,13 @@
 package meet_eat.data.entity;
 
 import meet_eat.data.Report;
+import meet_eat.data.factory.ReportFactory;
 import org.junit.Test;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class ReportableEntityCommonTest {
 
@@ -31,7 +33,18 @@ public class ReportableEntityCommonTest {
 
     @Test
     public void testConstructor() {
-        // TODO
+        // Test data
+        Collection<Report> reports = new LinkedList<>();
+        ReportFactory reportFactory = new ReportFactory();
+        Report report = reportFactory.getValidObject();
+        reports.add(report);
+        String identifier = "TestIdentifierXY";
+
+        // Execution
+        ConcreteReportableEntity entity = new ConcreteReportableEntity(identifier, reports);
+
+        // Assertions
+        assertTrue(entity.getReports().contains(report));
     }
 
     @Test(expected = NullPointerException.class)
@@ -51,12 +64,27 @@ public class ReportableEntityCommonTest {
 
     @Test
     public void testAddReport() {
-        // TODO
+        // Test data
+        Collection<Report> reports = new LinkedList<>();
+        ReportFactory reportFactory = new ReportFactory();
+        Report report1 = reportFactory.getValidObject();
+        Report report2 = reportFactory.getValidObject();
+        reports.add(report1);
+        reports.add(report2);
+
+        // Execution
+        ConcreteReportableEntity entity = new ConcreteReportableEntity();
+        entity.addReport(report1);
+        entity.addReport(report2);
+
+        // Assertions
+        assertFalse(report1.equals(report2));
+        assertTrue(entity.getReports().containsAll(reports));
     }
 
     @Test(expected = NullPointerException.class)
     public void testAddReportNull() {
-        // Excecution
+        // Execution
         ConcreteReportableEntity entity = new ConcreteReportableEntity();
         entity.addReport(null);
     }
