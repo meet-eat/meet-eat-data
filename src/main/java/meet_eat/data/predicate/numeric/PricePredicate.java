4fe83567e0d36a794f26798948665365d6e81d02
Moritz Gstuer <moritz@gstuer.com>
2020-08-04 00:27:31 +0200

    Add price predicate class

---
+++ src/main/java/meet_eat/data/predicate/numeric/PricePredicate.java
@@ -0,0 +1,22 @@
+package meet_eat.data.predicate.numeric;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import meet_eat.data.entity.Offer;
+import meet_eat.data.predicate.OfferPredicate;
+
+public class PricePredicate extends DoubleOperator implements OfferPredicate {
+
+    private static final long serialVersionUID = -9147038172467909868L;
+
+    @JsonCreator
+    protected PricePredicate(@JsonProperty("operation") DoubleOperation operation,
+                             @JsonProperty("referenceValue") double priceBound) {
+        super(operation, priceBound);
+    }
+
+    @Override
+    public boolean test(Offer offer) {
+        return operate(offer.getPrice());
+    }
+}
