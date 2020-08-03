0b16f10e0aecb5138b9b8dd3b280bb72c95b6450
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-04 00:10:31 +0200

    Add ReportCommonTest

---
+++ src/test/java/meet_eat/data/ReportCommonTest.java
@@ -0,0 +1,62 @@
+package meet_eat.data;
+
+import meet_eat.data.entity.user.User;
+import meet_eat.data.factory.ReportFactory;
+import meet_eat.data.factory.UserFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ReportCommonTest {
+
+    @Test
+    public void testConstructor() {
+        // Test data
+        UserFactory userFactory = new UserFactory();
+        User user = userFactory.getValidObject();
+        String message = "MyReportMessage";
+
+        // Execution
+        Report report = new Report(user, message);
+
+        // Assertions
+        assertNotNull(report);
+        assertEquals(user, report.getReporter());
+        assertEquals(message, report.getMessage());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorWithNullReporterAndMessage() {
+        // Test data
+        String message = "MyReportMessage";
+
+        // Execution
+        Report report = new Report(null, message);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorWithReporterAndNullMessage() {
+        // Test data
+        UserFactory userFactory = new UserFactory();
+        User user = userFactory.getValidObject();
+
+        // Execution
+        Report report = new Report(user, null);
+    }
+
+    @Test
+    public void testSetProcessed() {
+        // Test data
+        ReportFactory reportFactory = new ReportFactory();
+        Report report = reportFactory.getValidObject();
+        boolean processed = true;
+
+        // Execution
+        report.setProcessed(processed);
+
+        // Assertions
+        assertNotNull(report);
+        assertEquals(processed, report.isProcessed());
+    }
+}
