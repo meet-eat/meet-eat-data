9681b8e565a657faf94158aac19a1fd97380228f
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-04 22:44:14 +0200

    Update test email addresses

--- src/test/java/meet_eat/data/factory/EmailFactory.java
+++ src/test/java/meet_eat/data/factory/EmailFactory.java
@@ -6,6 +6,6 @@ public class EmailFactory extends ObjectFactory<Email> {
 
     @Override
     protected Email createObject() {
-        return new Email("test.address" + objectCounter + "@meet-eat.com");
+        return new Email("test.address" + objectCounter + "@example.com");
     }
 }
