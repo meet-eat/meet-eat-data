ffa5925c01c574c536f4ce21f1b8365596477f42
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-06 00:43:12 +0200

    Add javadoc for contact package

--- src/main/java/meet_eat/data/entity/user/contact/ContactData.java
+++ src/main/java/meet_eat/data/entity/user/contact/ContactData.java
@@ -10,8 +10,11 @@ import java.util.EnumMap;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Represents the information exchanged during a successful {@link ContactRequest}.
+ */
 public class ContactData {
-    
+
     private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
     private static final String ERROR_MESSAGE_NULL_REQUEST = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "request");
     private static final String ERROR_MESSAGE_NULL_CONTACT_TYPE = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "contactType");
@@ -22,32 +25,65 @@ public class ContactData {
     @JsonProperty
     private final ContactRequest request;
 
+    /**
+     * Creates contact data with no given {@link ContactType}.
+     *
+     * @param request the for the transmission necessary contact information for a {@link ContactRequest}
+     */
     public ContactData(ContactRequest request) {
-        contacts = new EnumMap<>(ContactType.class);
         this.request = Objects.requireNonNull(request, ERROR_MESSAGE_NULL_REQUEST);
+        contacts = new EnumMap<>(ContactType.class);
     }
 
+    /**
+     * Creates contact data.
+     *
+     * @param request  the for the transmission necessary contact information for a {@link ContactRequest}
+     * @param contacts the requested contact information
+     */
     @JsonCreator
     public ContactData(@JsonProperty("request") ContactRequest request, @JsonProperty("contacts") Map<ContactType, String> contacts) {
-        this.contacts = Objects.requireNonNull(contacts);
         this.request = Objects.requireNonNull(request, ERROR_MESSAGE_NULL_REQUEST);
+        this.contacts = Objects.requireNonNull(contacts);
     }
 
+    /**
+     * Gets the requested contact information.
+     *
+     * @return the requested contact information
+     */
     @JsonGetter
     public Map<ContactType, String> getContacts() {
         return Collections.unmodifiableMap(contacts);
     }
 
+    /**
+     * Gets the corresponding value of a {@link ContactType}.
+     *
+     * @param type the contact type
+     * @return the value
+     */
     @JsonIgnore
-    public String getContactByType(ContactType type){
+    public String getContactByType(ContactType type) {
         return contacts.get(type);
     }
 
+    /**
+     * Gets the {@link ContactRequest}.
+     *
+     * @return the contact request
+     */
     @JsonGetter
     public ContactRequest getRequest() {
         return request;
     }
 
+    /**
+     * Puts a contact type with corresponding value to the map.
+     *
+     * @param type  the contact type
+     * @param value the value
+     */
     public void putContact(ContactType type, String value) {
         Objects.requireNonNull(type, ERROR_MESSAGE_NULL_CONTACT_TYPE);
         Objects.requireNonNull(value, ERROR_MESSAGE_NULL_VALUE);
--- src/main/java/meet_eat/data/entity/user/contact/ContactRequest.java
+++ src/main/java/meet_eat/data/entity/user/contact/ContactRequest.java
@@ -7,8 +7,11 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import meet_eat.data.entity.user.User;
 
+/**
+ * Represents a contact request from a {@link User} to a {@link User}.
+ */
 public class ContactRequest {
-    
+
     private static final String ERROR_MESSAGE_TEMPLATE_NULL = "The %s must not be null.";
     private static final String ERROR_MESSAGE_NULL_REQUESTER = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "requester");
     private static final String ERROR_MESSAGE_NULL_REQUESTED_USER = String.format(ERROR_MESSAGE_TEMPLATE_NULL, "requestedUser");
@@ -18,17 +21,33 @@ public class ContactRequest {
     @JsonProperty
     private final User requestedUser;
 
+    /**
+     * Creates a contact request.
+     *
+     * @param requester     the requester
+     * @param requestedUser the requested user
+     */
     @JsonCreator
     public ContactRequest(@JsonProperty("requester") User requester, @JsonProperty("requestedUser") User requestedUser) {
-        this.requester = Objects.requireNonNull(requester, ERROR_MESSAGE_NULL_REQUESTER);;
-        this.requestedUser = Objects.requireNonNull(requestedUser, ERROR_MESSAGE_NULL_REQUESTED_USER);;
+        this.requester = Objects.requireNonNull(requester, ERROR_MESSAGE_NULL_REQUESTER);
+        this.requestedUser = Objects.requireNonNull(requestedUser, ERROR_MESSAGE_NULL_REQUESTED_USER);
     }
 
+    /**
+     * Gets the requester.
+     *
+     * @return the requester
+     */
     @JsonGetter
     public User getRequester() {
         return requester;
     }
 
+    /**
+     * Gets the requested user.
+     *
+     * @return the requested user
+     */
     @JsonGetter
     public User getRequestedUser() {
         return requestedUser;
