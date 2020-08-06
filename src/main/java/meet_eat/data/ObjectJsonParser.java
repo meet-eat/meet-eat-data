591ae234fe32b00c93cfdc0e073b1f77e542e225
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-06 03:00:24 +0200

    Add javadoc to ObjectJsonParser

--- src/main/java/meet_eat/data/ObjectJsonParser.java
+++ src/main/java/meet_eat/data/ObjectJsonParser.java
@@ -10,6 +10,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.util.Objects;
 
+/**
+ * Represents a class to serialize objects to its json representation and to deserialize JSON representations of objects
+ * to objects.
+ */
 public class ObjectJsonParser {
 
     private static final String ERROR_MESSAGE_STRING_NOT_PARSABLE = "Given json string must be parsable, but was not.";
@@ -17,14 +21,30 @@ public class ObjectJsonParser {
 
     private ObjectMapper objectMapper;
 
+    /**
+     * Creates a default object JSON parser.
+     */
     public ObjectJsonParser() {
         this.objectMapper = getDefaultObjectMapper();
     }
 
+    /**
+     * Creates an objects JSON parser by given {@link ObjectMapper}.
+     *
+     * @param objectMapper the object mapper
+     */
     public ObjectJsonParser(ObjectMapper objectMapper) {
         this.objectMapper = Objects.requireNonNull(objectMapper);
     }
 
+    /**
+     * Parses a JSON representation to an {@link Object}.
+     *
+     * @param jsonString the JSON representation
+     * @param type       the class name
+     * @param <T>        the class type
+     * @return the object represented by the JSON string if, and only if, the string is parsable
+     */
     public <T> T parseJsonStringToObject(String jsonString, Class<T> type) {
         try {
             return objectMapper.readValue(jsonString, type);
@@ -33,6 +53,14 @@ public class ObjectJsonParser {
         }
     }
 
+    /**
+     * Parses a JSON representation to an {@link Object}.
+     *
+     * @param jsonString the jsonString
+     * @param type       the java type
+     * @param <T>        the class type
+     * @return the object represented by the JSON string if, and only if, the string is parsable
+     */
     public <T> T parseJsonStringToObject(String jsonString, JavaType type) {
         try {
             return objectMapper.readValue(jsonString, type);
@@ -41,6 +69,12 @@ public class ObjectJsonParser {
         }
     }
 
+    /**
+     * Parses an {@link Object} to its JSON representation.
+     *
+     * @param object the object
+     * @return the JSON representation of the object if, and only if, the object is parsable
+     */
     public String parseObjectToJsonString(Object object) {
         try {
             return objectMapper.writeValueAsString(object);
@@ -49,14 +83,29 @@ public class ObjectJsonParser {
         }
     }
 
+    /**
+     * Gets the current {@link ObjectMapper}.
+     *
+     * @return the object mapper
+     */
     public ObjectMapper getObjectMapper() {
         return objectMapper;
     }
 
+    /**
+     * Sets the {@link ObjectMapper}.
+     *
+     * @param objectMapper the object mapper
+     */
     public void setObjectMapper(ObjectMapper objectMapper) {
         this.objectMapper = Objects.requireNonNull(objectMapper);
     }
 
+    /**
+     * Gets the default {@link ObjectMapper}.
+     *
+     * @return the default object mapper
+     */
     public static ObjectMapper getDefaultObjectMapper() {
         // Add JDK8 and java.time.* specific functionality to mapper using module registration.
         ObjectMapper objectMapper = new ObjectMapper()
