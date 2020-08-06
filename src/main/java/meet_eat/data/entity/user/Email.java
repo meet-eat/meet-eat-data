a50ae5622dae1984cca757026cfb47b40a1ce0dd
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-06 02:35:36 +0200

    Add javadoc to Email

--- src/main/java/meet_eat/data/entity/user/Email.java
+++ src/main/java/meet_eat/data/entity/user/Email.java
@@ -7,11 +7,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Represents an email address belonging to a {@link User}.
+ */
 public class Email implements Serializable {
 
     private static final long serialVersionUID = -4013709321180976803L;
 
     private static final String ERROR_MESSAGE_ILLEGAL_EMAIL = "The email address must comply with RFC 5322.";
+
+    /**
+     * The official RFC 5322 email pattern.
+     */
     private static final String REGEX_EMAIL_ADDRESS = "(?:[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+)"
             + "*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")"
             + "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])"
@@ -21,6 +28,11 @@ public class Email implements Serializable {
     @JsonProperty
     private final String address;
 
+    /**
+     * Creates an email address.
+     *
+     * @param address the email address
+     */
     @JsonCreator
     public Email(@JsonProperty("address") String address) {
         if (!isLegalEmailAddress(address)) {
@@ -29,6 +41,12 @@ public class Email implements Serializable {
         this.address = address;
     }
 
+    /**
+     * Checks the email address for correctness.
+     *
+     * @param address the email address
+     * @return {@code true} if the email address is legal, {@code false} otherwise
+     */
     public static boolean isLegalEmailAddress(String address) {
         if (Objects.isNull(address)) {
             return false;
