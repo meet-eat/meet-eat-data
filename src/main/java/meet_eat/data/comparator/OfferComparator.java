ce48013618f91f526f228dc0b4a95307399dfb81
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-06 20:55:25 +0200

    Add getter OfferComparator

--- src/main/java/meet_eat/data/comparator/OfferComparator.java
+++ src/main/java/meet_eat/data/comparator/OfferComparator.java
@@ -3,20 +3,30 @@ package meet_eat.data.comparator;
 import meet_eat.data.entity.Offer;
 import meet_eat.data.location.Localizable;
 import meet_eat.data.location.UnlocalizableException;
+import org.springframework.cglib.core.Local;
 
 import java.io.Serializable;
 import java.util.Comparator;
+import java.util.Objects;
 
 public class OfferComparator implements Serializable, Comparator<Offer> {
 
     private static final long serialVersionUID = 3846969499567330524L;
 
     private final OfferComparableField field;
-    private final Localizable localizable;
+    private final Localizable location;
 
-    public OfferComparator(OfferComparableField field, Localizable localizable) {
-        this.field = field;
-        this.localizable = localizable;
+    public OfferComparator(OfferComparableField field, Localizable location) {
+        this.field = Objects.requireNonNull(field);
+        this.location = Objects.requireNonNull(location);
+    }
+
+    public OfferComparableField getField() {
+        return field;
+    }
+
+    public Localizable getLocation() {
+        return location;
     }
 
     @Override
@@ -42,7 +52,7 @@ public class OfferComparator implements Serializable, Comparator<Offer> {
     private double getDistance(Localizable localizable) {
         double distance;
         try {
-            distance = localizable.getDistance(this.localizable);
+            distance = localizable.getDistance(location);
         } catch (UnlocalizableException exception) {
             distance = Double.MAX_VALUE;
         }
