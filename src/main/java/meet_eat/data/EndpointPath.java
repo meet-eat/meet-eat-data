967e1c908b7e382d0fd45df51e0bdc58a019f829
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-07 01:20:54 +0200

    Add javadoc for EndpointPath

--- src/main/java/meet_eat/data/EndpointPath.java
+++ src/main/java/meet_eat/data/EndpointPath.java
@@ -1,11 +1,37 @@
 package meet_eat.data;
 
+import meet_eat.data.entity.Offer;
+import meet_eat.data.entity.Tag;
+import meet_eat.data.entity.user.User;
+
+/**
+ * Represents descriptor of URI entity endpoint paths.
+ */
 public final class EndpointPath {
 
+    /**
+     * The URI endpoint path for: login
+     */
     public static final String LOGIN = "/login";
+
+    /**
+     * The URI endpoint path for: logout
+     */
     public static final String LOGOUT = "/logout";
+
+    /**
+     * The URI endpoint path for: {@link Tag}s
+     */
     public static final String TAGS = "/tags";
+
+    /**
+     * The URI endpoint path for: {@link Offer}s
+     */
     public static final String OFFERS = "/offers";
+
+    /**
+     * The URI endpoint path for: {@link User}s
+     */
     public static final String USERS = "/users";
 
     private EndpointPath() {
