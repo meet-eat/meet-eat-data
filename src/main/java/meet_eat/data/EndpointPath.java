b4c627c84064a61c56689c5811b38f17e7074e7e
Moritz Gstuer <moritz@gstuer.com>
2020-08-07 00:52:18 +0200

    Move endpoint paths to data project

---
+++ src/main/java/meet_eat/data/EndpointPath.java
@@ -0,0 +1,13 @@
+package meet_eat.data;
+
+public final class EndpointPath {
+
+    public static final String LOGIN = "/login";
+    public static final String LOGOUT = "/logout";
+    public static final String TAGS = "/tags";
+    public static final String OFFERS = "/offers";
+    public static final String USERS = "/users";
+
+    private EndpointPath() {
+    }
+}
