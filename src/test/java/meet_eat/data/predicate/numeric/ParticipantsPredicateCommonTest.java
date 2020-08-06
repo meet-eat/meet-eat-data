981480009a8a7439c3eb83702a411a32ce6464a7
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-06 21:55:22 +0200

    Add Predicate tests for numerics

---
+++ src/test/java/meet_eat/data/predicate/numeric/ParticipantsPredicateCommonTest.java
@@ -0,0 +1,55 @@
+package meet_eat.data.predicate.numeric;
+
+import meet_eat.data.entity.Offer;
+import meet_eat.data.factory.OfferFactory;
+import meet_eat.data.factory.UserFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParticipantsPredicateCommonTest {
+
+    @Test
+    public void testConstructor() {
+        // Test data
+        DoubleOperation operation = DoubleOperation.EQUAL;
+        int reference = 1;
+
+        // Execution
+        ParticipantsPredicate participantsPredicate = new ParticipantsPredicate(operation, reference);
+
+        // Assertions
+        assertNotNull(participantsPredicate);
+        assertNotNull(participantsPredicate.getOperation());
+        assertNotNull(participantsPredicate.getReferenceValue());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorWithNullOperation() {
+        // Test data
+        int reference = 1;
+
+        // Execution
+        ParticipantsPredicate participantsPredicate = new ParticipantsPredicate(null, reference);
+    }
+
+    @Test
+    public void testOperate() {
+        // Test data
+        DoubleOperation operation = DoubleOperation.EQUAL;
+        int reference = 2;
+        OfferFactory offerFactory = new OfferFactory();
+        Offer offerOne = offerFactory.getValidObject();
+        Offer offerTwo = offerFactory.getValidObject();
+
+        UserFactory userFactory = new UserFactory();
+        offerOne.addParticipant(userFactory.getValidObject());
+
+        // Execution
+        ParticipantsPredicate participantsPredicate = new ParticipantsPredicate(operation, reference);
+
+        // Assertions
+        assertFalse(participantsPredicate.test(offerOne));
+        assertTrue(participantsPredicate.test(offerTwo));
+    }
+}
---
+++ src/test/java/meet_eat/data/predicate/numeric/RatingPredicateCommonTest.java
@@ -0,0 +1,80 @@
+package meet_eat.data.predicate.numeric;
+
+import meet_eat.data.entity.Offer;
+import meet_eat.data.entity.user.rating.Rating;
+import meet_eat.data.entity.user.rating.RatingBasis;
+import meet_eat.data.entity.user.rating.RatingValue;
+import meet_eat.data.factory.OfferFactory;
+import meet_eat.data.factory.UserFactory;
+import meet_eat.data.predicate.string.DescriptionPredicate;
+import meet_eat.data.predicate.string.StringOperation;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RatingPredicateCommonTest {
+
+    @Test
+    public void testConstructor() {
+        // Test data
+        DoubleOperation operation = DoubleOperation.EQUAL;
+        double reference = 1d;
+
+        // Execution
+        RatingPredicate ratingPredicate = new RatingPredicate(operation, reference);
+
+        // Assertions
+        assertNotNull(ratingPredicate);
+        assertNotNull(ratingPredicate.getOperation());
+        assertNotNull(ratingPredicate.getReferenceValue());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorWithNullOperation() {
+        // Test data
+        double reference = 1d;
+
+        // Execution
+        RatingPredicate ratingPredicate = new RatingPredicate(null, reference);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testConstructorWithNullReferenceValue() {
+        // Test data
+        DoubleOperation operation = DoubleOperation.EQUAL;
+
+        // Execution
+        RatingPredicate ratingPredicate = new RatingPredicate(operation, null);
+    }
+
+    @Test
+    public void testOperate() {
+        // Test data
+        DoubleOperation operation = DoubleOperation.LESS;
+        double reference = 3.5;
+        OfferFactory offerFactory = new OfferFactory();
+        Offer offerOne = offerFactory.getValidObject();
+        Offer offerTwo = offerFactory.getValidObject();
+
+        UserFactory userFactory = new UserFactory();
+        // Average host rating: 3.6
+        offerOne.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_5, userFactory.getValidObject()));
+        offerOne.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_2, userFactory.getValidObject()));
+        offerOne.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_5, userFactory.getValidObject()));
+        offerOne.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_1, userFactory.getValidObject()));
+        offerOne.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_5, userFactory.getValidObject()));
+        // Average host rating: 3.4
+        offerTwo.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_4, userFactory.getValidObject()));
+        offerTwo.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_4, userFactory.getValidObject()));
+        offerTwo.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_2, userFactory.getValidObject()));
+        offerTwo.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_4, userFactory.getValidObject()));
+        offerTwo.getCreator().addRating(new Rating(RatingBasis.HOST, RatingValue.POINTS_3, userFactory.getValidObject()));
+
+        // Execution
+        RatingPredicate ratingPredicate = new RatingPredicate(operation, reference);
+
+        // Assertions
+        assertFalse(ratingPredicate.test(offerOne));
+        assertTrue(ratingPredicate.test(offerTwo));
+    }
+}
