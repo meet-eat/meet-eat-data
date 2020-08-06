ee0202ffccb3f512a50d22fbda4ab06fe95ffb64
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-06 03:11:45 +0200

    Add javadoc to RequestHeaderField

--- src/main/java/meet_eat/data/RequestHeaderField.java
+++ src/main/java/meet_eat/data/RequestHeaderField.java
@@ -1,10 +1,28 @@
 package meet_eat.data;
 
+/**
+ * Represents types which can be passed into request headers used for HTTP requests.
+ */
 public final class RequestHeaderField {
 
+    /**
+     * The string representation of a token.
+     */
     public static final String TOKEN = "token";
+
+    /**
+     * The string representation of a page.
+     */
     public static final String PAGE = "page";
+
+    /**
+     * The string representation of predicates.
+     */
     public static final String PREDICATES = "predicates";
+
+    /**
+     * The string representation of comparators.
+     */
     public static final String COMPARATORS = "comparators";
 
     private RequestHeaderField() {
