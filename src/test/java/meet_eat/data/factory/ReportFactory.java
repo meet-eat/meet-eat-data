5a5347c23a33886b5bfbb08c2958271d22c736e5
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2020-08-21 19:03:28 +0200

    Make ReportFactory deprecated
    The report is now an EntityRelation and thus no longer usable in its current state.

--- src/test/java/meet_eat/data/factory/ReportFactory.java
+++ src/test/java/meet_eat/data/factory/ReportFactory.java
@@ -3,6 +3,7 @@ package meet_eat.data.factory;
 import meet_eat.data.entity.relation.Report;
 import meet_eat.data.entity.user.User;
 
+@Deprecated
 public class ReportFactory extends ObjectFactory<Report> {
 
     private final UserFactory userFactory;
